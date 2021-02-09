0691af798fa1c89b5c2d22d0476243a1a2e3d7c3
Angel Valladares <arivalladares2.0@gmail.com>
2021-02-09 15:49:16 -0600

    Add files via upload

---
+++ kali.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+sudo apt install gnupg
+wget 'https://archive.kali.org/archive-key.asc'
+sudo apt-key add archive-key.asc
+sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+sudo apt update
+echo " "
+sudo apt-get update 
